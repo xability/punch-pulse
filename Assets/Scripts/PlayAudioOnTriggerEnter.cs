c9e5dd0ac8f1059347b8d0c5028c06dad6dc821f
Anurag Choudhary <157350443+anuragc3@users.noreply.github.com>
2024-08-30 12:03:33 -0500

    feat: added sounds for dummy, player punch action.
    Also, bunch of changes made while trying to deploy it on the Meta Quest device.

--- Assets/Scripts/PlayAudioOnTriggerEnter.cs
+++ Assets/Scripts/PlayAudioOnTriggerEnter.cs
@@ -8,6 +8,14 @@ public class PlayAudioOnTriggerEnter : MonoBehaviour
     private AudioSource source;
     public string targetTag;
 
+    public bool useVelocity = true;
+    public float minVelocity = 0;
+    public float maxVelocity = 2;
+
+    public bool randomizePitch = true;
+    public float minPitch = 0.0f;
+    public float maxPitch = 1.2f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +30,29 @@ public class PlayAudioOnTriggerEnter : MonoBehaviour
     {
         if (other.CompareTag(targetTag))
         {
-            source.PlayOneShot(clip);
+            VelocityEstimator estimator = other.GetComponent<VelocityEstimator>();
+
+            if(estimator && useVelocity)
+            {
+                float v = estimator.GetVelocityEstimate().magnitude;
+                float volume = Mathf.InverseLerp(minVelocity, maxVelocity, v);
+                if (randomizePitch)
+                {
+                    source.pitch = Random.Range(minPitch, maxPitch);
+                }
+                source.PlayOneShot(clip, volume);
+            }
+
+            else
+            {
+                if (randomizePitch)
+                {
+                    source.pitch = Random.Range(minPitch, maxPitch);
+                }
+
+                source.PlayOneShot(clip);
+            }
+            
         }
         
     }
